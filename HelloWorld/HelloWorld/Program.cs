7eb0ac16bee4773be29ee08d7a021cb2cc448b0b
Kyrylo Avramenko <v-kyavra@microsoft.com>
2019-02-28 11:28:47 -0800

    Change "press any key" writeLine

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -9,7 +9,7 @@ namespace HelloWorld
             Console.WriteLine("Goodbye cruel World!");
             Console.WriteLine();
 
-            Console.WriteLine("Press any key to exit...");
+            Console.WriteLine("Wrong line...");
             Console.ReadKey();
         }
     }
