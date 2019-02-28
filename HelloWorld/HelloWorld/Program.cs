d8027c9343ed944dce3ab13f117285ac3b346955
Kyrylo Avramenko <v-kyavra@microsoft.com>
2019-02-28 11:22:50 -0800

    add "cruel" word to "Goodbye world" string

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Goodbye World!");
+            Console.WriteLine("Goodbye cruel World!");
             Console.WriteLine();
 
             Console.WriteLine("Press any key to exit...");
